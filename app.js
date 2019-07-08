d3a72206ba0f85a63b8371a2f25bf9b555a23f66
git config --global user.email xxx@xx.comhacker.行者无疆@DESKTOP-MVF7FCF MINGW64 /f/黑马程序35期/35期案列/06.03 (master)$ git config --global user.name <xxx@xx.com>
2019-07-08 21:06:41 +0800

    第一天,页面等搭建完成

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //引入express模板
 const express = require("express");
+//引入ejs模板
+const ejs = require("ejs");
 //引入路由模块
 const router = require("./router/router.js");
 //创建应用
 const app = express();
 //添加对指定端口的监听
+//设置ejs模板引擎
+app.set("view engine", "ejs");
+//指定模板文件的目录,后期使用ejs的时候就可以参照这个目录进行ejs文件查询
+app.set("views", "views");
 app.listen(3000, (req, res) => {
   console.log("http://127.0.0.1:3000");
 });
--- router/router.js
+++ router/router.js
@@ -2,13 +2,17 @@
 //引入express
 const express = require("express");
 //引入user控制器模块
-const userControllers = require("../controllers/userControllers");
-//引入fs
-const fs = require("fs");
+const pagesControllers = require("../controllers/pagesControllers");
+
 //引入router
 //router()构造函数创建的路由对象可以当成一个中间件来使用
 const router = express.Router();
-
-router.get("/", userControllers.getIndexPage);
+//前台页面
+router
+  .get("/", pagesControllers.getIndexPage)
+  .get("/list", pagesControllers.getListPage)
+  .get("/detail", pagesControllers.getDetailPage)
+  //后台页面
+  .get("/admin", pagesControllers.getAdminPage);
 //暴露路由模块
 module.exports = router;
